5bd3b3bb5b013b6a50f2de3e3da406a976c27c1e
JeisonYamarte <jeisonyamarte16@gmail.com>
2025-04-22 00:19:20 -0400

    reestructuracion del server, compartido en carpetas segun su funcion y estadarizado

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import config from './config/env.js';
+import webhookRoutes from './routes/webhookRoutes.js';
+
+const app = express();
+app.use(express.json());
+
+app.use('/', webhookRoutes);
+
+app.get('/', (req, res)=>{
+    res.send(`<pre> Nothing to see here.
+        checkout README.md to start.</pre>`);
+});
+
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port: ${config.PORT}`);
+});
